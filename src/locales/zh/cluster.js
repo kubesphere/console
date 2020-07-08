40b01ff348ed25b3ad73078d81f79d261f5acb6c
Ray Zhou <ray@yunify.com>
2020-07-08 17:24:26 +0800

    Chore(cn): minor update

--- src/locales/zh/cluster.js
+++ src/locales/zh/cluster.js
@@ -107,7 +107,7 @@ export default {
 
   NO_CLUSTER_TIP: '请添加至少 1 个集群',
   NO_CLUSTER_TIP_DESC:
-    '集群是一组运行着 Kubernetes 的节点（物理或者虚拟机）, Kubesphere 的功能也依托于集群中的节点来运行',
+    '集群是一组运行着 Kubernetes 的节点 (物理或者虚拟机)，Kubesphere 的功能也依托于集群中的节点来运行',
   ADD_NEW_CLUSTER_DESC: '添加新的 Kubernetes 集群',
   CHOOSE_PROVIDER_DESC:
     'KubeSphere 提供了在主流服务商中快速部署 Kubernetes 集群的方案',
