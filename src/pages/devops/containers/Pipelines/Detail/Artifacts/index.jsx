aa5a3dcb780947b21d76278247daeade08a1caaf
harrisonliu5 <harrisonliu_5@163.com>
2020-07-21 14:31:28 +0800

    fix: Fix the download URL of the artifacts in the single cluster
    Signed-off-by: harrisonliu5 <harrisonliu_5@163.com>

--- src/pages/devops/containers/Pipelines/Detail/Artifacts/index.jsx
+++ src/pages/devops/containers/Pipelines/Detail/Artifacts/index.jsx
@@ -71,9 +71,11 @@ export default class Artifacts extends React.Component {
 
   getDownloadUrl = url => {
     const { params } = this.props.match
-    return `/kapis/clusters/${
-      params.cluster
-    }/devops.kubesphere.io/v1alpha2/jenkins${url}`
+    return params.cluster === 'default' || !params.cluster
+      ? `/kapis/devops.kubesphere.io/v1alpha2/jenkins${url}`
+      : `/kapis/clusters/${
+          params.cluster
+        }/devops.kubesphere.io/v1alpha2/jenkins${url}`
   }
 
   getFilteredValue = dataIndex => this.store.list.filters[dataIndex]
