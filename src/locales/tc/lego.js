91711f3091c99592ada41de4674f577dce4b9e3a
Sherlock <sherlockxu@yunify.com>
2020-07-27 18:29:04 +0800

    update lego file for es and tc and fix cap key
    Signed-off-by: Sherlock <sherlockxu@yunify.com>

---
+++ src/locales/tc/lego.js
@@ -0,0 +1,91 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export default {
+  LEGO_UI_OK: ' 确定',
+  LEGO_UI_CANCEL: '取消',
+  LEGO_UI_LOADING: '加载中...',
+  LEGO_UI_ASCENDING_ORDER: '正序排列…',
+  LEGO_UI_DESCENDING_ORDER: '倒序排列…',
+  LEGO_UI_HIDDEN: '隐藏…',
+  LEGO_UI_OPERATE: '操作',
+  LEGO_UI_FILTER: '过滤选项',
+  LEGO_UI_TIPS: '提示',
+  LEGO_UI_NO_RESULTS_FOUND: '未找到内容',
+  LEGO_UI_LOAD_MORE: '加载更多',
+  LEGO_UI_LOADING_PLACEHOLDER: '正在加载数据',
+  LEGO_UI_ASYNC_NO_RESULT: '已加载全部数据',
+  LEGO_UI_CLEAR_ALL: '清空所有',
+  LEGO_UI_CLEAR_VALUE: '清空当前值',
+  LEGO_UI_PLEASE_SELECT: '请选择',
+  LEGO_UI_REACH_BOTTOM: '已到底部',
+  LEGO_UI_LOAD_ITEMS: '查看更多',
+  LEGO_UI_NO_RESULT_LOG: '没有更多日志了',
+  LEGO_UI_PER_PAGE: '条/页',
+  LEGO_UI_GO_TO: ' 跳至',
+  LEGO_UI_PAGE: '页',
+  LEGO_UI_TOTAL_SUMMARY: '共有 {total} 条数据',
+  LEGO_UI_CANCEL_SELECT: '取消选择',
+  LEGO_UI_SELECTED_HELP: '已选择 {count} 条数据',
+  LEGO_UI_NO_AVAILABLE_DATA: '暂无数据',
+  LEGO_UI_SUN: '日',
+  LEGO_UI_MON: '一',
+  LEGO_UI_TUE: '二',
+  LEGO_UI_WED: '三',
+  LEGO_UI_THU: '四',
+  LEGO_UI_FRI: '五',
+  LEGO_UI_SAT: '六',
+  LEGO_UI_SUNDAY: '星期日',
+  LEGO_UI_MONDAY: '星期一',
+  LEGO_UI_TUESDAY: '星期二',
+  LEGO_UI_WEDNESDAY: '星期三',
+  LEGO_UI_THURSDAY: '星期四',
+  LEGO_UI_FRIDAY: '星期五',
+  LEGO_UI_SATURDAY: '星期六',
+  LEGO_UI_JAN: '1月',
+  LEGO_UI_FEB: '2月',
+  LEGO_UI_MAR: '3月',
+  LEGO_UI_APR: '4月',
+  LEGO_UI_MAY: '5月',
+  LEGO_UI_JUN: '6月',
+  LEGO_UI_JUL: '7月',
+  LEGO_UI_AUG: '8月',
+  LEGO_UI_SEP: '9月',
+  LEGO_UI_OCT: '10月',
+  LEGO_UI_NOV: '11月',
+  LEGO_UI_DEC: '12月',
+  LEGO_UI_JAN_LH: '一月',
+  LEGO_UI_FEB_LH: '二月',
+  LEGO_UI_MAR_LH: '三月',
+  LEGO_UI_APR_LH: '四月',
+  LEGO_UI_MAY_LH: '五月',
+  LEGO_UI_JUN_LH: '六月',
+  LEGO_UI_JUL_LH: '七月',
+  LEGO_UI_AUG_LH: '八月',
+  LEGO_UI_SEP_LH: '九月',
+  LEGO_UI_OCT_LH: '十月',
+  LEGO_UI_NOV_LH: '十一月',
+  LEGO_UI_DEC_LH: '十二月',
+  LEGO_UI_AM: '上午',
+  LEGO_UI_PM: '下午',
+  LEGO_UI_WEEK_ABBREVIATION: '周',
+  LEGO_UI_RANGE_SEPARATOR: ' 至 ',
+  LEGO_UI_SCROLL_TO_INCREMENT: '点击切换 12/24 小时时制',
+  LEGO_UI_CLICK_TO_TOGGLE: '滚动切换',
+  LEGO_UI_YEAR: '年',
+}
