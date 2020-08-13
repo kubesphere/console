515954d64c66395f1fd2320da3a14b4701114e27
leoliu <leoliu@yunify.com>
2020-08-13 10:39:57 +0800

    fix: Omit version in service selector
    Signed-off-by: leoliu <leoliu@yunify.com>

--- src/components/Forms/Workload/ContainerSettings/index.jsx
+++ src/components/Forms/Workload/ContainerSettings/index.jsx
@@ -16,7 +16,7 @@
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { concat, get, set, unset, isEmpty, omitBy, has } from 'lodash'
+import { concat, get, set, unset, isEmpty, omit, omitBy, has } from 'lodash'
 import React from 'react'
 import { safeParseJSON, generateId } from 'utils'
 import { MODULE_KIND_MAP } from 'utils/constants'
@@ -324,7 +324,11 @@ export default class ContainerSetting extends React.Component {
     )
 
     set(serviceTemplate, 'metadata.labels', labels)
-    set(serviceTemplate, `${serivcePrefix}spec.selector`, podLabels)
+    set(
+      serviceTemplate,
+      `${serivcePrefix}spec.selector`,
+      omit(podLabels, 'version')
+    )
 
     const placement = get(this.formTemplate, 'spec.placement')
     if (placement) {
