dfc1b58fedfbf9bfb4ec383192d67bcb4a7f6dd4
leoliu <leoliu@yunify.com>
2020-08-25 17:27:31 +0800

    fix: Fix log collection detail info
    Signed-off-by: leoliu <leoliu@yunify.com>

--- src/pages/clusters/containers/LogCollections/Detail/Configuration/index.jsx
+++ src/pages/clusters/containers/LogCollections/Detail/Configuration/index.jsx
@@ -30,21 +30,30 @@ import styles from './index.scss'
 export default class Configuration extends React.Component {
   render() {
     const detail = get(this.props.detailStore, 'detail', {})
-    const { address, config = {} } = detail
+    const { address, config = {}, type } = detail
 
     return (
       <Panel>
         <Attributes className={styles.attributes}>
+          {type === 'kafka' && (
+            <Attributes.Item
+              className={styles.item}
+              name={t('topic')}
+              value={config.topics}
+            />
+          )}
           <Attributes.Item
             className={styles.item}
             name={t('Address')}
             value={address}
           />
-          <Attributes.Item
-            className={styles.item}
-            name={t('Index Prefix')}
-            value={config.logstashPrefix}
-          />
+          {type === 'es' && (
+            <Attributes.Item
+              className={styles.item}
+              name={t('Index Prefix')}
+              value={config.logstashPrefix}
+            />
+          )}
         </Attributes>
       </Panel>
     )
