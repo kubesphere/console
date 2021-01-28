35e000f923f9c1f464d855c02573b0365408630d
ydFu <ader.ydfu@gmail.com>
2021-01-28 14:15:38 +0800

    Update Traditional Chinese translations
    * The commit rephrased some words and sentences for Traditional Chinese translations
    
    Signed-off-by: ydFu <ader.ydfu@gmail.com>

--- locales/tc/login.js
+++ locales/tc/login.js
@@ -17,38 +17,38 @@
  */
 
 module.exports = {
-  'Log In': '登录',
-  'Please Log In': '欢迎登录',
-  'Username or Email': '用户名或者邮箱',
-  Password: '密码',
-  Captcha: '验证码',
-  'User does not exist': '用户名不存在',
-  'Wrong username or password, please try again': '用户名或密码错误',
-  'Unable to access the backend services': '无法访问后端服务',
-  'Unable to access the api server': '无法访问 API 服务器',
+  'Log In': '登入',
+  'Please Log In': '歡迎登入',
+  'Username or Email': '用戶名或者信箱',
+  Password: '密碼',
+  Captcha: '驗證碼',
+  'User does not exist': '用戶名不存在',
+  'Wrong username or password, please try again': '用戶名或密碼錯誤',
+  'Unable to access the backend services': '無法訪問後端服務',
+  'Unable to access the api server': '無法訪問 API 服務器',
 
-  'Internal Server Error': '服务器内部错误',
-  'Please input the correct captcha': '验证码错误',
+  'Internal Server Error': '服務器內部錯誤',
+  'Please input the correct captcha': '驗證碼錯誤',
 
   Reload: '刷新',
-  'Reload Captcha': '刷新验证码',
+  'Reload Captcha': '刷新驗證碼',
 
-  INTERNAL_SERVER_ERROR_TIP: '对不起，服务器开小差了',
+  INTERNAL_SERVER_ERROR_TIP: '對不起，服務器有小差錯了',
   'Too many failed login attempts, please wait!':
-    '登录失败次数过多，请稍后重试',
-  'Log In with {title}': '通过 {title} 登录',
+    '登入失敗次數過多，請稍後重試',
+  'Log In with {title}': '通過 {title} 登入',
 
-  'Please input username or email': '请输入用户名或邮箱',
-  'Please input password': '请输入密码',
-  'Please input captch': '请输入验证码',
-  'Please confirm your account info': '请确认您的帐户信息',
+  'Please input username or email': '請輸入用戶名或信箱',
+  'Please input password': '請輸入密碼',
+  'Please input captch': '請輸入驗證碼',
+  'Please confirm your account info': '請確認您的帳戶資訊',
 
-  'Please reset your password': '请重置您的密码',
+  'Please reset your password': '請重置您的密碼',
 
-  'Modify Later': '稍后修改',
+  'Modify Later': '稍後修改',
 
   'Your password must meet the following requirements':
-    '您的密码必须符合如下要求',
+    '您的密碼必须符合如下要求',
 
-  CONFIRM_PASSWORD_TIP: '由于您是第一次登陆，处于安全考虑请重置您的密码',
+  CONFIRM_PASSWORD_TIP: '由于您是第一次登入，處於安全考慮請重置您的密碼',
 }
