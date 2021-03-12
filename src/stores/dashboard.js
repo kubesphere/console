759d2ae8c71fb58380807468385f06952f8d0bfe
leoliu <leoliu@yunify.com>
2021-03-12 18:14:29 +0800

    update: Update custom monitoring apis
    Signed-off-by: leoliu <leoliu@yunify.com>

--- src/stores/dashboard.js
+++ src/stores/dashboard.js
@@ -16,77 +16,8 @@
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { action } from 'mobx'
 import Base from 'stores/base'
-import List from 'stores/federated.list'
 
 export default class DashboardStore extends Base {
   module = 'dashboards'
-
-  list = new List()
-
-  getListUrl = ({ cluster, namespace }) =>
-    `${this.apiVersion}${this.getPath({ cluster, namespace })}/${this.module}`
-
-  @action
-  async fetchList({
-    cluster,
-    namespace,
-    page,
-    name,
-    limit,
-    more,
-    ...rest
-  } = {}) {
-    this.list.isLoading = true
-
-    page = Number(page)
-    if (!page || page === 1) {
-      this.list.continues = {}
-      page = 1
-    }
-
-    if (page > 1 && !this.list.continues[page]) {
-      page = 1
-      this.list.continues = {}
-    }
-
-    const params = rest
-
-    params.limit = limit || this.list.limit
-
-    if (this.list.continues[page]) {
-      params.continue = this.list.continues[page]
-    }
-
-    if (name) {
-      params.fieldSelector = `metadata.name=${name}`
-    }
-
-    const result = await request.get(
-      this.getListUrl({ cluster, namespace }),
-      params,
-      null,
-      () => {}
-    )
-
-    const data = result.items.map(item => ({
-      ...this.mapper(item),
-      cluster,
-    }))
-
-    this.list.continues[page + 1] = result.metadata.continue
-
-    if (page === 1) {
-      this.list.total = data.length + (result.metadata.remainingItemCount || 0)
-    }
-
-    this.list.update({
-      data: more ? [...this.list.data, ...data] : data,
-      page,
-      name,
-    })
-
-    this.list.isLoading = false
-  }
 }
