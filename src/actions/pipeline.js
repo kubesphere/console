201b50d1288caa65bf5f0224ae75af070819a46f
harrisonliu5 <harrisonliu_5@163.com>
2021-04-16 19:26:31 +0800

    chore: Remove Update Notice when the pipeline run
    Signed-off-by: harrisonliu5 <harrisonliu_5@163.com>

--- src/actions/pipeline.js
+++ src/actions/pipeline.js
@@ -237,7 +237,6 @@ export default {
           })
 
           Modal.close(modal)
-          Notify.success({ content: `${t('Updated Successfully')}` })
           success && success(branch)
         },
         store,
