ab8922778f7711b5e447989eea32262406ee33f8
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:43:36 +0800

    New translations configmap.js (Chinese Traditional)

--- locales/tc/configmap.js
+++ locales/tc/configmap.js
@@ -15,21 +15,18 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
-  configmaps: '配置',
-  ConfigMaps: '配置',
+  'Config Field': '配置項',
+  'Config Value': '配置項',
   ConfigMap: '配置',
   'ConfigMap Settings': '配置設置',
-  'Config Field': '配置項',
-  CONFIGMAP_DESC:
-    '配置集 (ConfigMap) 常用於儲存工作負載所需的配置資訊，許多應用程序會從配置文件、命令行參數或環境變量中讀取配置資訊。',
-  CONFIGMAP_CREATE_DESC:
-    '配置是儲存通用的配置變量的，使用戶可以將分布式系統中用於不同模組的環境變量統一到一個對象中管理',
-  'Config Value': '配置項',
+  configmaps: '配置',
+  ConfigMaps: '配置',
+  'Enter the value of the configuration map entry or use the contents of the file': '輸入配置映射條目的值或使用文件的内容',
   'Modify Config': '修改配置',
-  'The unique key value of this configuration map entry':
-    '此配置映射條目的唯一 key 值',
-  'Enter the value of the configuration map entry or use the contents of the file':
-    '輸入配置映射條目的值或使用文件的内容',
-}
+  'The unique key value of this configuration map entry': '此配置映射條目的唯一 key 值',
+  CONFIGMAP_CREATE_DESC: '配置是儲存通用的配置變量的，使用戶可以將分布式系統中用於不同模組的環境變量統一到一個對象中管理',
+  CONFIGMAP_DESC: '配置集 (ConfigMap) 常用於儲存工作負載所需的配置資訊，許多應用程序會從配置文件、命令行參數或環境變量中讀取配置資訊。',
+  CONFIGMAPS_BASEINFO_DESC: '',
+  CONFIGMAP_SETTINGS_DESC: ''
+};
