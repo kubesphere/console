4610389ca88644c647d560713b87c0eae373d4c7
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:43:59 +0800

    New translations app_template.js (Chinese Traditional)

--- locales/tc/app_template.js
+++ locales/tc/app_template.js
@@ -15,23 +15,16 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
-  'Deploy Successfully': '部署成功',
-  'Update Successfully': '更新成功',
-  'Configuration Files': '配置文件',
-
-  APP_TEMPLATE_DESCRIPTION_TITLE: '應用一鍵部署',
-  APP_TEMPLATE_DESCRIPTION_ARTICLE:
-    '通過視覺化的方式在 KubeSphere 中展示並提供部署及管理功能，用戶可以基於應用模板快速地一鍵部署應用',
-
   'App README': '應用介紹',
-
+  'Configuration Files': '配置文件',
+  'Deploy Successfully': '部署成功',
+  'Edit by YAML': '編輯 YAML',
+  'The app has no chart file.': '該應用沒有配置文件',
   'The app has no documentation.': '該應用沒有說明文件',
   'The version has no documentation.': '該版本沒有說明文件',
-  'The app has no chart file.': '該應用沒有配置文件',
-
-  TOTAL_APP_REPOS: '共計 {num} 個應用倉庫',
-
-  'Edit by YAML': '編輯 YAML',
-}
+  'Update Successfully': '更新成功',
+  APP_TEMPLATE_DESCRIPTION_TITLE: '應用一鍵部署',
+  APP_TEMPLATE_DESCRIPTION_ARTICLE: '通過視覺化的方式在 KubeSphere 中展示並提供部署及管理功能，用戶可以基於應用模板快速地一鍵部署應用',
+  TOTAL_APP_REPOS: '共計 {num} 個應用倉庫'
+};
