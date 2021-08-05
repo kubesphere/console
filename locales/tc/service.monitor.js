c1f091a395e421eb0cdde88446b60f53b99ab8d2
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:43:00 +0800

    New translations service.monitor.js (Chinese Traditional)

--- locales/tc/service.monitor.js
+++ locales/tc/service.monitor.js
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
   'Service Monitoring Exporter': '服務監控導出器',
   'Target Service': '目標服務',
@@ -23,16 +22,12 @@ module.exports = {
   'Scrap Interval(min)': '採集間隔(分鐘)',
   'View Monitor': '查看監控',
   'Monitoring Exporter': '監控導出器',
-
   'Choose Authentication Method': '選擇授權方式',
   'Port connection authentication': '端口連接授權',
   'No Auth': '無須認證',
-
   'Create a new secret': '建創新密鑰',
   'refresh secrets': '刷新密鑰',
-
   NO_AUTH_TIP: '接口可直接連接，無須授權',
-
   SCRAP_INTERVAL_DESC: '監控數據採集區間隔，默認為 1 分鐘',
-  SCRAP_TIMEOUT_DESC: '超時，默認值 10 秒',
-}
+  SCRAP_TIMEOUT_DESC: '超時，默認值 10 秒'
+};
