ec14fefa0261b722b9013a54cbeffd1f6f25dfeb
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:44:41 +0800

    New translations group.js (Chinese Traditional)

--- locales/tc/group.js
+++ locales/tc/group.js
@@ -15,16 +15,16 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
   'Add the member to': '添加成員到 <strong>{group}<strong>',
   'Department Name': '部門名稱',
+  'Department Alias': '别名',
   principal: '負責人',
   'Add New Department': '添加新的部門',
   'Add New Subdepartment': '添加新的子部門',
   NO_DEPARTMENT_TIP: '暫時沒有部門，請在右側創建',
-  'workspace role': '企業空間角色',
-  WORKSPACE_ROLE_DESC: '將角色賦予給整個用戶組',
+  'Workspace role': 'Workspace Role',
+  GROUP_WORKSPACE_ROLE_DESC: '企業空間角色將授予部門中的所有用戶。',
   'Bind Project Role': '綁定項目角色',
   'Add Project': '添加項目',
   'project roles': '項目角色',
@@ -32,11 +32,6 @@ module.exports = {
   'Add DevOps Project': '添加 DevOps 工程',
   'Please add role': '請添加角色',
   'User Group Assigned': '已分配用戶組',
-  'Department Alias': '别名',
-  GROUP_WORKSPACE_ROLE_DESC: '企業空間角色將授予部門中的所有用戶。',
-
-  DELETE_PARENT_GROUP_TIP:
-    '確定刪除子部門 <strong>{group_name}</strong>? 刪除該部門的同時，其子部門也會被刪除，且所有成員的授權也將被取消。',
-  DELETE_GROUP_TIP:
-    '確定刪除子部門 <strong>{group_name}</strong>? 刪除該部門的同時，所有成員的授權也將被取消。',
-}
+  DELETE_GROUP_TIP: '確定刪除子部門 <strong>{group_name}</strong>? 刪除該部門的同時，所有成員的授權也將被取消。',
+  DELETE_PARENT_GROUP_TIP: '確定刪除子部門 <strong>{group_name}</strong>? 刪除該部門的同時，其子部門也會被刪除，且所有成員的授權也將被取消。'
+};
