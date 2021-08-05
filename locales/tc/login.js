e9b40d12aba4e497404e85e10363c9090d48f93d
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:44:22 +0800

    New translations login.js (Chinese Traditional)

--- locales/tc/login.js
+++ locales/tc/login.js
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
   'Log In': '登入',
   'Please Log In': '歡迎登入',
@@ -26,30 +25,20 @@ module.exports = {
   'Wrong username or password, please try again': '用戶名或密碼錯誤',
   'Unable to access the backend services': '無法訪問後端服務',
   'Unable to access the api server': '無法訪問 API 服務器',
-
   'Internal Server Error': '服務器內部錯誤',
   'Please input the correct captcha': '驗證碼錯誤',
-
   Reload: '刷新',
   'Reload Captcha': '刷新驗證碼',
-
   INTERNAL_SERVER_ERROR_TIP: '對不起，服務器有小差錯了',
-  'Too many failed login attempts, please wait!':
-    '登入失敗次數過多，請稍後重試',
+  'Too many failed login attempts, please wait!': '登入失敗次數過多，請稍後重試',
   'Log In with {title}': '通過 {title} 登入',
-
   'Please input username or email': '請輸入用戶名或信箱',
   'Please input password': '請輸入密碼',
   'Please input captch': '請輸入驗證碼',
   'Please confirm your account info': '請確認您的帳戶資訊',
   'Please input username': '請輸入用戶名',
-
   'Please reset your password': '請重置您的密碼',
-
   'Modify Later': '稍後修改',
-
-  'Your password must meet the following requirements':
-    '您的密碼必须符合如下要求',
-
-  CONFIRM_PASSWORD_TIP: '由于您是第一次登入，處於安全考慮請重置您的密碼',
-}
+  'Your password must meet the following requirements': '您的密碼必须符合如下要求',
+  CONFIRM_PASSWORD_TIP: '由于您是第一次登入，處於安全考慮請重置您的密碼'
+};
