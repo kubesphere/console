94df414876d0d33dbb5269c25a4f64ceb5d46415
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:44:02 +0800

    New translations crd.js (Chinese Traditional)

--- locales/tc/crd.js
+++ locales/tc/crd.js
@@ -15,13 +15,9 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
   'Custom Resource Definition': '自定義資源 (CRD)',
   'Custom Resource Definitions': '自定義資源 (CRD)',
-
-  CUSTOM_RESOURCE_DEFINITION_DESC:
-    '自定義資源 (CRD) 是一種 Kubernetes 實現自定義資源類型的擴展方式，用戶可以如同操作内置資源對象一樣操作 CRD 對象。',
-  CUSTOM_RESOURCE_DEFINITION_CREATE_DESC:
-    '自定義資源 (CRD) 是一種 Kubernetes 實現自定義資源類型的擴展方式，用戶可以如同操作内置資源對象一樣操作 CRD 對象。',
-}
+  CUSTOM_RESOURCE_DEFINITION_DESC: '自定義資源 (CRD) 是一種 Kubernetes 實現自定義資源類型的擴展方式，用戶可以如同操作内置資源對象一樣操作 CRD 對象。',
+  CUSTOM_RESOURCE_DEFINITION_CREATE_DESC: '自定義資源 (CRD) 是一種 Kubernetes 實現自定義資源類型的擴展方式，用戶可以如同操作内置資源對象一樣操作 CRD 對象。'
+};
