43d650dd1bcdaba5fc1d39512399bd45dc4f30d0
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:44:40 +0800

    New translations group.js (Chinese Simplified)

--- locales/zh/group.js
+++ locales/zh/group.js
@@ -15,28 +15,23 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
-  'Add the member to': '添加成员到 <strong>{group}<strong>',
-  'Department Name': '部门名称',
-  principal: '负责人',
-  'Add New Department': '添加新的部门',
-  'Add New Subdepartment': '添加新的子部门',
-  NO_DEPARTMENT_TIP: '暂时没有部门，请在右侧创建',
-  'Workspace role': '企业空间角色',
-  WORKSPACE_ROLE_DESC: '将角色赋予给整个用户组',
-  'Bind Project Role': '绑定项目角色',
-  'Add Project': '添加项目',
-  'project roles': '项目角色',
-  'Bind DevOps Project Role': '绑定 DevOps 工程角色',
-  'Add DevOps Project': '添加 DevOps 工程',
-  'Please add role': '请添加角色',
-  'User Group Assigned': '已分配用户组',
-  'Department Alias': '别名',
-  GROUP_WORKSPACE_ROLE_DESC: '企业空间角色将授予部门中的所有用户。',
-
-  DELETE_PARENT_GROUP_TIP:
-    '确定删除子部门 <strong>{group_name}</strong>? 删除该部门的同时，其子部门也会被删除，且所有成员的授权也将被取消。',
-  DELETE_GROUP_TIP:
-    '确定删除子部门 <strong>{group_name}</strong>? 删除该部门的同时，所有成员的授权也将被取消。',
-}
+  'Add the member to': 'Are you sure you want to assign the user to <strong>{group}</strong>?',
+  'Department Name': 'Name',
+  'Department Alias': 'Alias',
+  principal: 'principal',
+  'Add New Department': 'Create Department',
+  'Add New Subdepartment': 'Create Department',
+  NO_DEPARTMENT_TIP: 'No department available. Please create a department on the right.',
+  'Workspace role': 'Workspace Role',
+  GROUP_WORKSPACE_ROLE_DESC: 'The workspace role will be assigned to all members in the department.',
+  'Bind Project Role': 'Project Role',
+  'Add Project': 'Add Project',
+  'project roles': 'project roles',
+  'Bind DevOps Project Role': 'DevOps Project Role',
+  'Add DevOps Project': 'Add DevOps Project',
+  'Please add role': 'Please select a role.',
+  'User Group Assigned': 'Department',
+  DELETE_GROUP_TIP: 'Are you sure you want to delete the department <strong>{group_name}</strong>? The associated roles will be unbound from the users.',
+  DELETE_PARENT_GROUP_TIP: 'Are you sure you want to delete the department <strong>{group_name}</strong>? Its subdepartments will also be deleted and the associated roles will be unbound from the users.'
+};
