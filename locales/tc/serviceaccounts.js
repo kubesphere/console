b7c37895271be651d0606902bc71f4ab050c114f
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:43:05 +0800

    New translations serviceaccounts.js (Chinese Traditional)

--- locales/tc/serviceaccounts.js
+++ locales/tc/serviceaccounts.js
@@ -15,18 +15,13 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
   ServiceAccounts: '服務帳戶',
   ServiceAccount: '服務帳戶',
   'Edit Service Account': '編輯服務帳戶',
   'Modify Service Account Role': '修改服務帳戶角色',
   'Secret Detail': '密鑰詳情',
-  SERVICEACCOUNT_DESC:
-    '服務帳戶（Service Account）為 Pod 中運行的進程提供了壹個標識，用於訪問 API Server。',
-
+  SERVICEACCOUNT_DESC: '服務帳戶（Service Account）為 Pod 中運行的進程提供了壹個標識，用於訪問 API Server。',
   'Invalid configuration file format': '配置文件格式錯誤',
-
-  SERVICEACCOUNT_KUBECONFIG_DESC:
-    'KubeConfig 配置方法請參照 <a href="https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/" target="_black">官方文檔</a> ，下載後請將服務地址修改為 Kubernetes API 的外部地址',
-}
+  SERVICEACCOUNT_KUBECONFIG_DESC: 'KubeConfig 配置方法請參照 <a href="https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/" target="_black">官方文檔</a> ，下載後請將服務地址修改為 Kubernetes API 的外部地址'
+};
