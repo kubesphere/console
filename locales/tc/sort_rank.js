0714f4fc66d0c8b36e7cee6d958677a9145ef44d
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:43:13 +0800

    New translations sort_rank.js (Chinese Traditional)

--- locales/tc/sort_rank.js
+++ locales/tc/sort_rank.js
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
   node_cpu_utilisation: 'CPU 使用率',
   node_load1: 'CPU 平均負載',
@@ -23,13 +22,11 @@ module.exports = {
   node_disk_size_utilisation: '本地儲存用量',
   node_pod_utilisation: '容器組用量',
   node_disk_inode_utilisation: 'inode 使用率',
-
   'Sort By node_cpu_utilisation': '按 CPU 使用率排行',
   'Sort By node_memory_utilisation': '按記憶體使用率排行',
   'Sort By node_disk_size_utilisation': '按本地儲存用量排行',
   'Sort By node_pod_utilisation': '按容器組用量排行',
   'Sort By node_disk_inode_utilisation': '按 inode 使用率排行',
-
   'Sort By node_load1': '按 CPU 平均負載排行',
   'Sort By namespace_memory_usage_wo_cache': '按記憶體使用量排行',
   'Sort By namespace_cpu_usage': '按 CPU 使用量排行',
@@ -45,5 +42,5 @@ module.exports = {
   'Sort By workload_cpu_usage': '按 CPU 使用量排行',
   'Sort By workload_memory_usage_wo_cache': '按記憶體使用量排行',
   'Sort By workload_net_bytes_transmitted': '按網路流出速率排行',
-  'Sort By workload_net_bytes_received': '按網路流入速率排行',
-}
+  'Sort By workload_net_bytes_received': '按網路流入速率排行'
+};
