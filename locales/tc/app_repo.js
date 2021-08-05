9ab2780c6351bbe0aec9a4d5871f4f743faef77f
Zhengyi Lai <laizhyi@gmail.com>
2021-08-05 16:43:51 +0800

    New translations app_repo.js (Chinese Traditional)

--- locales/tc/app_repo.js
+++ locales/tc/app_repo.js
@@ -15,47 +15,38 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 module.exports = {
-  Validate: '驗證',
-  APP_REPO_DESC:
-    '可用添加來自第三方的應用倉庫源，可以支持基於 Helm 的 Kubernetes 應用。 ',
-  APP_REPO_URL_DESC: '所輸入的 URL 需要先驗證才可進行添加或編輯操作',
+  'Add App Repository': '添加應用倉庫',
+  'Add Repository': '添加倉庫',
+  'App Repos': '應用倉庫',
   'App Repository': '應用倉庫',
   'App Repository Name': '應用倉庫名稱',
-  'Add App Repository': '添加應用倉庫',
   'Edit App Repository': '編輯應用倉庫',
-  APP_REPOSITORY_CREATE_DESC:
-    '可用添加來自第三方的應用倉庫源，可以支持基於 Helm 的 Kubernetes 應用。 ',
-
-  Synchronize: '同步',
-  syncing: '同步中',
-
-  'Unrecognized URL': '無法識別的地址',
+  'HTTP access denied': 'HTTP 地址無法訪問',
+  'Index Repo': '同步倉庫',
   'Invalid format of credential': '憑證格式錯誤',
-  'Missing access key ID': '缺少 Access key ID',
-  'Missing secret access key': '缺少 Secret access key',
-  'S3 access denied': 'S3 地址無法訪問',
   'Invalid format of URL': '地址格式錯誤',
   'Invalid HTTP Scheme': '錯誤的 HTTP 類型協定',
-  'HTTP access denied': 'HTTP 地址無法訪問',
   'Invalid HTTPS Scheme': '錯誤的 HTTPS 類型協定',
-  'Invalid type': '無效的類型',
   'Invalid Providers': '錯誤的 Providers 參數',
   'Invalid Repo URL': '不是有效的 Repo 地址',
   'Invalid S3 Scheme': '錯誤的 S3 類型協定',
+  'Invalid type': '無效的類型',
   'Invalid URL': '無效的 URL',
   'Valid URL': 'URL 驗證通过',
-
+  'Missing access key ID': '缺少 Access key ID',
+  'Missing secret access key': '缺少 Secret access key',
+  'Please input access key ID and secret access key': '請輸入 Access key ID 和 Secret access key',
   'Please input URL': '請輸入 URL',
-  'Please input access key ID and secret access key':
-    '請輸入 Access key ID 和 Secret access key',
-
-  'App Repos': '應用倉庫',
-  'Add Repository': '添加倉庫',
-  'Index Repo': '同步倉庫',
-
+  'S3 access denied': 'S3 地址無法訪問',
+  Synchronize: '同步',
+  syncing: '同步中',
+  'Unrecognized URL': '無法識別的地址',
+  Validate: '驗證',
+  'App Repositorys': 'App Repositories',
+  APP_REPO_URL_DESC: '所輸入的 URL 需要先驗證才可進行添加或編輯操作',
+  APP_REPOSITORY_CREATE_DESC: '可用添加來自第三方的應用倉庫源，可以支持基於 Helm 的 Kubernetes 應用。 ',
+  APP_REPO_DESC: '可用添加來自第三方的應用倉庫源，可以支持基於 Helm 的 Kubernetes 應用。 ',
   HOW_TO_USE_APP_REPO_Q: '如何使用應用倉庫？',
-  HOW_TO_USE_APP_REPO_A:
-    '企業空間内添加的應用倉庫可以在企業空間下的項目内可見，可以通過 應用 => 部署應用 =>  應用倉庫 中查找到目前應用倉庫下的所有應用,並進行部署',
-}
+  HOW_TO_USE_APP_REPO_A: '企業空間内添加的應用倉庫可以在企業空間下的項目内可見，可以通過 應用 => 部署應用 =>  應用倉庫 中查找到目前應用倉庫下的所有應用,並進行部署'
+};
