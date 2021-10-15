f64ba6529e94b3cb46f3f43d29a235788c602ef7
harrisonliu5 <harrisonliu@kubesphere.io>
2021-10-15 18:54:03 +0800

    fix: Modify the apiextensions api
    Signed-off-by: harrisonliu5 <harrisonliu@kubesphere.io>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -879,7 +879,7 @@ export const API_VERSIONS = {
   strategies: 'apis/servicemesh.kubesphere.io/v1alpha2',
   servicepolicies: 'apis/servicemesh.kubesphere.io/v1alpha2',
   horizontalpodautoscalers: 'apis/autoscaling/v2beta2',
-  customresourcedefinitions: 'apis/apiextensions.k8s.io/v1beta1',
+  customresourcedefinitions: 'apis/apiextensions.k8s.io/v1',
   clusters: 'apis/cluster.kubesphere.io/v1alpha1',
   workspaces: 'apis/tenant.kubesphere.io/v1alpha2',
   users: 'apis/iam.kubesphere.io/v1alpha2',
