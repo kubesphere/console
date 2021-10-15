9a0a0cfb5816b84083bd21814dcc267e49237b3d
harrisonliu5 <harrisonliu@kubesphere.io>
2021-10-15 17:13:28 +0800

    fix: Fix the name of the auto create router have error namespace
    Signed-off-by: harrisonliu5 <harrisonliu@kubesphere.io>

--- src/pages/projects/components/Modals/RouteRulesEdit/index.jsx
+++ src/pages/projects/components/Modals/RouteRulesEdit/index.jsx
@@ -137,7 +137,7 @@ class RouteRulesEdit extends React.Component {
   }
 
   render() {
-    const { visible, cluster, isSubmitting } = this.props
+    const { visible, cluster, isSubmitting, namespace } = this.props
     const { subRoute, formTemplate } = this.state
 
     return (
@@ -158,6 +158,7 @@ class RouteRulesEdit extends React.Component {
             formRef={this.form}
             formTemplate={formTemplate}
             cluster={cluster}
+            namespace={namespace}
           />
         </div>
       </Modal>
