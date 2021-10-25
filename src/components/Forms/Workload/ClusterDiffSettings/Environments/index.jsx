2fdc9b489a5089c9f42ae869da8698afc68b0122
harrisonliu5 <harrisonliu@kubesphere.io>
2021-10-25 16:34:42 +0800

    fix: Fix the env of fed-project
    Signed-off-by: harrisonliu5 <harrisonliu@kubesphere.io>

--- src/components/Forms/Workload/ClusterDiffSettings/Environments/index.jsx
+++ src/components/Forms/Workload/ClusterDiffSettings/Environments/index.jsx
@@ -19,49 +19,18 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 
-import ConfigMapStore from 'stores/configmap'
-import SecretStore from 'stores/secret'
-
 import EditForm from '../EditForm'
 import Environments from '../../ContainerSettings/ContainerForm/Environments'
 
 @observer
 export default class ContainerImages extends Component {
-  state = {
-    configMaps: [],
-    secrets: [],
-  }
-
-  configMapStore = new ConfigMapStore()
-
-  secretStore = new SecretStore()
-
-  componentDidMount() {
-    this.fetchData()
-  }
-
-  fetchData() {
-    const { cluster, namespace } = this.props
-
-    Promise.all([
-      this.configMapStore.fetchListByK8s({ cluster, namespace }),
-      this.secretStore.fetchListByK8s({ cluster, namespace }),
-    ]).then(([configMaps, secrets]) => {
-      this.setState({
-        configMaps,
-        secrets,
-      })
-    })
-  }
-
   handleSubmit = data => {
     const { index, onEdit } = this.props
     onEdit({ index, data })
   }
 
   render() {
-    const { formData } = this.props
-    const { configMaps, secrets } = this.state
+    const { formData, clusters, namespace } = this.props
 
     const title = (
       <span>{`${t('ENVIRONMENT_VARIABLE_PL')}: ${(formData.env || [])
@@ -72,9 +41,10 @@ export default class ContainerImages extends Component {
     return (
       <EditForm {...this.props} title={title} onOk={this.handleSubmit}>
         <Environments
-          configMaps={configMaps}
-          secrets={secrets}
           checkable={false}
+          isFederated={true}
+          namespace={namespace}
+          projectDetail={{ clusters }}
         />
       </EditForm>
     )
