84ebe0141f8ee641d64c9dd36b140f83ca7fac36
mango <xu.weikyrie@foxmail.com>
2021-11-15 15:44:41 +0800

    fix error of code scanning
    Signed-off-by: mango <xu.weiKyrie@foxmail.com>

--- src/utils/object.mapper.js
+++ src/utils/object.mapper.js
@@ -1120,10 +1120,9 @@ const FederatedMapper = resourceMapper => item => {
 
 const DevOpsMapper = item => {
   const phase = get(item, 'status.phase')
-  const syncStatus = get(
-    item,
+  const syncStatusKey =
     'metadata.annotations["devopsproject.devops.kubesphere.io/syncstatus"]'
-  )
+
   const deletionTimestamp = get(item, 'metadata.deletionTimestamp')
 
   return {
@@ -1132,7 +1131,9 @@ const DevOpsMapper = item => {
     devops: get(item, 'metadata.name'),
     workspace: get(item, 'metadata.labels["kubesphere.io/workspace"]'),
     namespace: get(item, 'status.adminNamespace'),
-    status: deletionTimestamp ? 'Terminating' : phase || syncStatus || 'Active',
+    status: deletionTimestamp
+      ? 'Terminating'
+      : phase || get(item, syncStatusKey) || 'Active',
     _originData: getOriginData(item),
   }
 }
