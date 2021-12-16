7cb026b039afafc8e4417382fe7877b14ac496d0
TheYoungManLi <cjl@kubesphere.io>
2021-12-16 13:43:19 +0800

    fix: Deleted pv still in the table list
    Signed-off-by: TheYoungManLi <cjl@kubesphere.io>

--- src/pages/clusters/containers/Storage/Volumes/Volume/index.jsx
+++ src/pages/clusters/containers/Storage/Volumes/Volume/index.jsx
@@ -253,9 +253,9 @@ export default class Volumes extends React.Component {
     const renderModeTip = (
       <div>
         <div>{t('ACCESS_MODE_TCAP')}:</div>
-        <div>RWO (ReadWriteOnce)：{t('ACCESS_MODE_RWO')}</div>
-        <div>ROX (ReadOnlyMany)：{t('ACCESS_MODE_ROX')}</div>
-        <div>RWX (ReadWriteMany)：{t('ACCESS_MODE_RWX')}</div>
+        <div>RWO (ReadWriteOnce): {t('ACCESS_MODE_RWO')}</div>
+        <div>ROX (ReadOnlyMany): {t('ACCESS_MODE_ROX')}</div>
+        <div>RWX (ReadWriteMany): {t('ACCESS_MODE_RWX')}</div>
       </div>
     )
     return (
