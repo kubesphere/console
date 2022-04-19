44deddbaa2eba4373404fda3c752ed3a74f59b65
harrisonliu5 <harrisonliu@kubesphere.io>
2022-04-19 11:18:07 +0800

    fix: Fix the annotations in gateway modal
    Signed-off-by: harrisonliu5 <harrisonliu@kubesphere.io>

--- src/pages/projects/components/Modals/GatewaySetting/index.jsx
+++ src/pages/projects/components/Modals/GatewaySetting/index.jsx
@@ -78,11 +78,30 @@ export default class GatewaySettingModal extends React.Component {
       ),
       configError: '',
     }
+  }
 
+  componentDidMount() {
+    this.initAnnotations()
+  }
+
+  getTypeOptions = () => [
+    { label: 'NodePort', value: 'NodePort' },
+    { label: 'LoadBalancer', value: 'LoadBalancer' },
+  ]
+
+  initAnnotations = () => {
     const annotations = get(this.template, 'spec.service.annotations')
     const type = get(this.template, 'spec.service.type')
+    const annotationType = get(
+      this.template,
+      "metadata.annotations['kubesphere.io/annotations']"
+    )
 
-    if (isEmpty(annotations) && type === 'LoadBalancer') {
+    if (
+      isEmpty(annotations) &&
+      type === 'LoadBalancer' &&
+      annotationType === 'QingCloud Kubernetes Engine'
+    ) {
       set(
         this.template,
         'spec.service.annotations',
@@ -90,11 +109,6 @@ export default class GatewaySettingModal extends React.Component {
       )
     }
   }
-
-  getTypeOptions = () => [
-    { label: 'NodePort', value: 'NodePort' },
-    { label: 'LoadBalancer', value: 'LoadBalancer' },
-  ]
 
   handleOk = () => {
     const { onOk } = this.props
@@ -150,7 +164,11 @@ export default class GatewaySettingModal extends React.Component {
 
   handleAnnotations = value => {
     this.options = Object.keys(CLUSTER_PROVIDERS_ANNOTATIONS[value])
-    this.setAnnotations({})
+    if (value === 'QingCloud Kubernetes Engine') {
+      this.setAnnotations(globals.config.loadBalancerDefaultAnnotations)
+    } else {
+      this.setAnnotations({})
+    }
   }
 
   setAnnotations = value => {
