e5963711b1f5af01816db8fc1aad55458f2d40c4
zhoutian <zhoutian@yunify.com>
2022-08-19 17:21:17 +0800

    optimization the image building of kubesphere/console
    Signed-off-by: zhoutian <zhoutian@yunify.com>

--- hack/docker_build_multiarch.sh
+++ hack/docker_build_multiarch.sh
@@ -19,9 +19,29 @@ fi
 # supported platforms
 PLATFORMS=linux/amd64,linux/arm64
 
+#first preimage
+${CONTAINER_CLI} build \
+  -f build/Dockerfile \
+  --target builder
+  -t ks-console-pre:"${TAG}" .
+
+#build preimage
+${CONTAINER_CLI} create \
+  --name predbuild ks-console-pre:
+
+#copy preimage:./out/ ./out/
+${CONTAINER_CLI} cp \
+  predbuild:/out/ ./out/
+
 # shellcheck disable=SC2086 # inteneded splitting of CONTAINER_BUILDER
 ${CONTAINER_CLI} ${CONTAINER_BUILDER} \
   --platform ${PLATFORMS} \
   ${PUSH} \
-  -f build/Dockerfile \
+  -f build/DockerfileFinal \
   -t "${REPO}"/ks-console:"${TAG}" .
+
+#delete preimage
+docker rmi ks-console-pre -f
+
+#delete ./out folder
+rm -rf ./out
