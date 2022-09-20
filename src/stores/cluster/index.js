cea6a3098e7f7127800d9c63389aa0eddd6e0d7b
TheYoungManLi <cjl@kubesphere.io>
2022-09-20 16:55:07 +0800

    fix: Update api version
    Signed-off-by: TheYoungManLi <cjl@kubesphere.io>

--- src/stores/cluster/index.js
+++ src/stores/cluster/index.js
@@ -54,7 +54,9 @@ export default class ClusterStore extends Base {
     `kapis/cluster.kubesphere.io/v1alpha1/clusters/${cluster}/agent/deployment`
 
   getTenantUrl = (params = {}) =>
-    `kapis/tenant.kubesphere.io/v1alpha2${this.getPath(params)}/${this.module}`
+    `kapis/resources.kubesphere.io/v1alpha3${this.getPath(params)}/${
+      this.module
+    }`
 
   @action
   async fetchList({ from, more, ...params } = {}) {
