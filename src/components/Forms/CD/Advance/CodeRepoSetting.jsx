617728dca29a71d71501501c02330b44b92923af
胡亚洲 <yazhouhu@yunify.com>
2023-01-17 22:21:05 +0800

    fix:Fix issue 872, add more information for continuous deployment parameters (#4069)
    * fix: issue 872, add more information for continuous deployment parameters
    
    Signed-off-by: 胡亚洲 <yazhouhu@yunify.com>
    
    * feat: remove zh annotation
    
    Signed-off-by: 胡亚洲 <yazhouhu@yunify.com>
    
    Signed-off-by: 胡亚洲 <yazhouhu@yunify.com>

---
+++ src/components/Forms/CD/Advance/CodeRepoSetting.jsx
@@ -0,0 +1,56 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react'
+import { Form, Input, Column, Columns } from '@kube-design/components'
+
+import CodeRepoSelector from '../../../CodeRepoSelector'
+
+export default class Advance extends React.Component {
+  render() {
+    const { formRef, formTemplate, devops, cluster } = this.props
+    // TODO: v3.4 directory recurse
+    return (
+      <Form data={formTemplate} ref={formRef}>
+        <Form.Item
+          label={t('CODE_REPOSITORY')}
+          rules={[{ required: true, message: t('REPO_EMPTY_DESC') }]}
+        >
+          <CodeRepoSelector name="repoURL" devops={devops} cluster={cluster} />
+        </Form.Item>
+        <Form.Item>
+          <Columns>
+            <Column>
+              <Form.Item label={t('REVISION')} desc={t('REVISIONS_DESC')}>
+                <Input name="source.targetRevision" defaultValue="HEAD" />
+              </Form.Item>
+            </Column>
+            <Column>
+              <Form.Item
+                label={t('MANIFEST_FILE_PATH')}
+                desc={t('MANIFEST_FILE_PATH_DESC')}
+              >
+                <Input name="source.path" defaultValue="." />
+              </Form.Item>
+            </Column>
+          </Columns>
+        </Form.Item>
+      </Form>
+    )
+  }
+}
