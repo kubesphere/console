0afbb5b5f660c6150e2fc19a47b58cc7f654870d
yazhou <yazhouhu@yunify.com>
2023-05-30 15:28:16 +0800

    fix: remove same locale
    Signed-off-by: yazhou <yazhouhu@yunify.com>

--- locales/pt/l10n-multiclusterProjects-applicationWorkloads-apps-list.js
+++ locales/pt/l10n-multiclusterProjects-applicationWorkloads-apps-list.js
@@ -23,6 +23,4 @@ module.exports = {
   // List > Create > Service Settings > Cluster Differences
   NAME_VALUE: 'Name: {value}',
   CONTAINER_PORT_VALUE: 'Container Port: {value}'
-  //alert ???
-  ACTIVATION_CONDITION_SCAP: 'Activation condition',
 }
