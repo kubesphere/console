68ac5b2732a8547510f0aabb5eeb2b5a5c8efc9e
fcl <63338728+fuchunlan@users.noreply.github.com>
2023-09-04 18:16:55 +0800

    feat: Cherry pick LADP (#4187)
    * feat: LDAP login page optimization (#841)
    
    Signed-off-by: miaqiang <qiangmin@yunify.com>
    Co-authored-by: miaqiang <qiangmin@yunify.com>
    feat: Cherry pick LADP
    Signed-off-by: lannyfu <lannyfu@kubesphere.io>
    
    * style: Cherry pick login page style
    
    Signed-off-by: lannyfu <lannyfu@kubesphere.io>
    
    * fix: Delete replace paltform logo commit
    
    Signed-off-by: lannyfu <lannyfu@kubesphere.io>
    
    ---------
    
    Signed-off-by: miaqiang <qiangmin@yunify.com>
    Signed-off-by: lannyfu <lannyfu@kubesphere.io>
    Co-authored-by: miaqiang <758477879@qq.com>
    Co-authored-by: miaqiang <qiangmin@yunify.com>

--- server/services/session.js
+++ server/services/session.js
@@ -82,6 +82,19 @@ const login = async (data, headers) => {
   return handleLoginResp(resp)
 }
 
+const loginThird = async ({ title, ...params }) => {
+  const resp = await send_gateway_request({
+    method: 'POST',
+    url: `/oauth/login/${title}`,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+    params,
+  })
+
+  return handleLoginResp(resp)
+}
+
 const getNewToken = async ctx => {
   const refreshToken = ctx.cookies.get('refreshToken')
   let newToken = {}
@@ -412,7 +425,8 @@ const getOAuthInfo = async () => {
       if (item && item.provider) {
         let url
         let params = {}
-        let type
+        const title = item.name
+        let type = item.type
         let endSessionURL
 
         const authURL = get(item, 'provider.endpoint.authURL')
@@ -449,8 +463,8 @@ const getOAuthInfo = async () => {
                 `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
             )
             .join('&')}`
-          servers.push({ title: item.name, url, type, endSessionURL })
         }
+        servers.push({ title, type, url, endSessionURL })
       }
     })
   }
@@ -478,6 +492,7 @@ const createUser = (params, token) => {
 
 module.exports = {
   login,
+  loginThird,
   oAuthLogin,
   getCurrentUser,
   getOAuthInfo,
