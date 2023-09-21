1d6aa353236f45cac494e37be2cc300e45b4255d
Liu Xin <harrisonliu@kubesphere.io>
2023-09-21 15:34:26 +0800

    doc: fix the poc description (#4195)
    Signed-off-by: harrisonliu5 <harrisonliu@kubesphere.io>
    Signed-off-by: wangyao <904190290@qq.com>

--- locales/zh/l10n-multiclusterProjects-persistentVolumeClaims-details.js
+++ locales/zh/l10n-multiclusterProjects-persistentVolumeClaims-details.js
@@ -23,7 +23,8 @@ module.exports = {
   // More > Edit YAML
   // More > Delete
   // Resource Status
-  VOLUME_MONITORING_TIP: '目前仅支持采集已挂载卷的用量数据，并且 OpenEBS、Local PV、NFS 等路径型卷的用量数据通常与实际值有一定出入。<a href="https://github.com/kubesphere/kubesphere/issues/2921" target="_blank">了解更多</a>',
+  VOLUME_MONITORING_TIP:
+    '存储系统需要具备真实的 volume quota 控制能力才可以输出真实准确的数据，基于 host path, local pv, 开源 nfs server 构建的存储系统通常不具备该能力。<a href="https://github.com/kubesphere/kubesphere/issues/2921" target="_blank">了解更多</a>',
   // Mount Information
-  MOUNT_INFORMATION: '挂载信息'
-};
+  MOUNT_INFORMATION: '挂载信息',
+}
