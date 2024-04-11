5cef966fc3b04703e5911b389aa59a864f3a0bfa
古月 <git@yazhou.io>
2024-04-11 15:04:53 +0800

    fix: Fix gitlab source repo options error (#4270)
    * fix: Fix gitlab source repo options error
    
    Signed-off-by: 古月 <git@yazhou.io>
    
    * chore: Update yarn.lock
    
    Signed-off-by: 古月 <git@yazhou.io>
    
    ---------
    
    Signed-off-by: 古月 <git@yazhou.io>

--- src/components/Forms/Pipelines/RepoSelect/GitLabForm/index.jsx
+++ src/components/Forms/Pipelines/RepoSelect/GitLabForm/index.jsx
@@ -104,7 +104,7 @@ export default class GitLabForm extends React.Component {
   render() {
     const { formData, credentials } = this.props.store
     const { formRef } = this.props
-    const { serverList } = this.state
+    const { serverList, projectList } = this.state
 
     return (
       <div className={styles.card}>
@@ -166,11 +166,7 @@ export default class GitLabForm extends React.Component {
           >
             <Select
               name="gitlab_source.repo"
-              options={this.state.projectList.map(item => {
-                const owner = get(formData, 'gitlab_source.owner')
-                const repo = item.replace(`${owner}/`, '')
-                return { label: repo, value: repo }
-              })}
+              options={projectList}
               placeholder=" "
               clearable
             />
