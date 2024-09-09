094e685dd6dcd9d340a1a334b0d950f60cdfbdd3
liujian <54946465+redscholar@users.noreply.github.com>
2024-09-09 15:49:53 +0800

    fix: sync image to aliyuns repo
    Signed-off-by: joyceliu <joyceliu@yunify.com>

--- hack/docker_build_multiarch.sh
+++ hack/docker_build_multiarch.sh
@@ -5,16 +5,11 @@ set -o pipefail
 
 TAG=${TAG:-latest}
 REPO=${REPO:-kubespheredev}
-PUSH=${PUSH:-}
+DRY_RUN=${DRY_RUN:-false}
 
 # support other container tools. e.g. podman
 CONTAINER_CLI=${CONTAINER_CLI:-docker}
 CONTAINER_BUILDER=${CONTAINER_BUILDER:-"buildx build"}
-
-# If set, just building, no pushing
-if [[ -z "${DRY_RUN:-}" ]]; then
-  PUSH="--push"
-fi
 
 ## supported platforms
 PLATFORMS=linux/amd64,linux/arm64
@@ -39,10 +34,11 @@ mv "$PROJECT_DIR"/server/locales \
   "$PROJECT_DIR"/server/sample \
   "$PROJECT_DIR"/server/configs "$PROJECT_DIR"/out/server/
 
-# shellcheck disable=SC2086 # inteneded splitting of CONTAINER_BUILDER
-${CONTAINER_CLI} ${CONTAINER_BUILDER} \
-  --platform ${PLATFORMS} \
-  ${PUSH} \
-  -f build/Dockerfile.dapper \
-  -t "${REPO}"/ks-console:"${TAG}" .
-  
+if [ "$DRY_RUN" = "false" ]; then
+  # shellcheck disable=SC2086 # inteneded splitting of CONTAINER_BUILDER
+  ${CONTAINER_CLI} ${CONTAINER_BUILDER} \
+    --platform ${PLATFORMS} \
+    --push \
+    -f build/Dockerfile.dapper \
+    -t "${REPO}"/ks-console:"${TAG}" .
+fi
