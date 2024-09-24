d1693387aa31d268be32b019d1e1036d33f39ab6
donniean <12584040+donniean@users.noreply.github.com>
2024-09-24 17:09:31 +0800

    feat: add isKsEdition in ENV (#5192)
    Signed-off-by: donniean <donniean1@gmail.com>

--- packages/shared/src/constants/env.ts
+++ packages/shared/src/constants/env.ts
@@ -8,3 +8,5 @@ export const isDevelopment = process.env.NODE_ENV === 'development';
 export const isProduction = process.env.NODE_ENV === 'production';
 
 export const isKseEdition = process.env.KUBESPHERE_EDITION === 'kse';
+
+export const isKsEdition = process.env.KUBESPHERE_EDITION === 'ks';
