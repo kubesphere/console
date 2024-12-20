ed3a2a71e8f347d9e2219eef218c9406a6c2682f
donniean <donniean1@gmail.com>
2024-12-20 11:01:39 +0800

    feat(secret): replace Button with StyledButton for improved styling in ImagerRegistry component
    Signed-off-by: donniean <donniean1@gmail.com>

---
+++ packages/console/src/pages/clusters/components/SecretSettings/ImagerRegistry/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Button } from '@kubed/components';
+
+export const StyledButton = styled(Button)`
+  width: auto !important;
+  min-width: 72px;
+  padding: 0;
+`;
