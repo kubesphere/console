81c41276e6d90c13b9461ac27e965b8d1bf03e27
donniean <donniean1@gmail.com>
2024-12-23 17:31:18 +0800

    chore: remove useless WorkspaceSelectorModal in packages/console
    Signed-off-by: donniean <donniean1@gmail.com>

--- packages/console/src/pages/workspaces/components/Modals/index.ts
+++ packages/console/src/pages/workspaces/components/Modals/index.ts
@@ -6,7 +6,6 @@
 export { default as HelmUploadModal } from './HelmUploadModal';
 export { default as RepoManagementModal } from './RepoManagementModal';
 export { default as EditAppModal } from './EditAppModal';
-export { default as WorkspaceSelectorModal } from './WorkspaceSelectorModal';
 export * from './CreateWorkspaceModal';
 export * from './EditWorkspaceModal';
 export * from './DeleteWorkspaceModal';
