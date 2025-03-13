f2667d53d05c9288fd6f1a43211692f2a0b65b35
donniean <12584040+donniean@users.noreply.github.com>
2025-03-13 16:05:50 +0800

    feat: add customizable text for create button in DataTable component (#4506)
    Signed-off-by: donniean <donniean1@gmail.com>

--- packages/shared/src/components/DataTable/TableEmpty/CreateButton.tsx
+++ packages/shared/src/components/DataTable/TableEmpty/CreateButton.tsx
@@ -4,18 +4,25 @@
  */
 
 import React from 'react';
-import type { ReactElement, MouseEvent } from 'react';
+import type { ReactNode, ReactElement, MouseEvent } from 'react';
 
 import { StyledButton } from './CreateButton.styles';
 
 export interface CreateButtonProps {
   createButton?: boolean | ReactElement;
+  createButtonText?: ReactNode;
   clickCreateButtonFn?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
-export function CreateButton({ createButton, clickCreateButtonFn }: CreateButtonProps) {
+export function CreateButton({
+  createButton,
+  createButtonText,
+  clickCreateButtonFn,
+}: CreateButtonProps) {
   if (createButton === true) {
-    return <StyledButton onClick={clickCreateButtonFn}>{t('CREATE')}</StyledButton>;
+    return (
+      <StyledButton onClick={clickCreateButtonFn}>{createButtonText ?? t('CREATE')}</StyledButton>
+    );
   }
 
   if (!createButton) {
