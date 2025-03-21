ca2943acd2cd3efde860688ee7ade25dc75bed77
donniean <donniean1@gmail.com>
2025-03-21 15:23:48 +0800

    refactor: replace checkForbiddenError middleware with checkForbidden and update route handling for 403 responses
    Signed-off-by: donniean <donniean1@gmail.com>

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,7 @@ const bodyParser = require('koa-bodyparser');
 const proxy = require('./middlewares/proxy');
 const checkToken = require('./middlewares/checkToken');
 const checkIfExist = require('./middlewares/checkIfExist');
-const checkForbiddenError = require('./middlewares/checkForbiddenError');
+const checkForbidden = require('./middlewares/checkForbidden');
 
 const { getServerConfig } = require('./libs/utils');
 
@@ -60,8 +60,8 @@ router
   .use(proxy('/b2i_download/(.*)', b2iFileProxy))
   .get('/blank_md', renderMarkdown)
 
-  .all('/proxy-api/(.*)', checkToken, checkForbiddenError, checkIfExist)
-  .all('(/clusters/[^/]*)?/(k?)api(s?)/(.*)', checkToken, checkForbiddenError, checkIfExist)
+  .all('/proxy-api/(.*)', checkToken, checkIfExist)
+  .all('(/clusters/[^/]*)?/(k?)api(s?)/(.*)', checkToken, checkForbidden, checkIfExist)
   .use(proxy('/apis/marketplace/(.*)', marketplaceApiProxy))
   .use(proxy('(/clusters/[^/]*)?/(k?)api(s?)/(.*)', k8sResourceProxy))
   .use(proxy('/proxy/(.*)', k8sResourceProxy))
