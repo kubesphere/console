2104732e3dbfae0f73d074c28acbf288c1f9c43c
donniean <donniean1@gmail.com>
2025-03-26 16:03:49 +0800

    refactor: replace 403 error page with ErrorLayout component for improved structure and maintainability
    Signed-off-by: donniean <donniean1@gmail.com>

--- packages/core/src/containers/Errors/components/ErrorLayout/styles.tsx
+++ packages/core/src/containers/Errors/components/ErrorLayout/styles.tsx
@@ -21,12 +21,12 @@ export const Container = styled.div`
   box-shadow: 0 4px 8px 0 rgba(36, 46, 66, 0.06);
 `;
 
-export const Illustration = styled.div`
+export const Illustration = styled.div<{ $backgroundImagePath: string }>`
   width: 320px;
   height: 180px;
   background-repeat: no-repeat;
   background-size: 100% 100%;
-  background-image: url('/assets/403.svg');
+  background-image: url('${({ $backgroundImagePath }) => $backgroundImagePath}');
 `;
 
 export const Title = styled.h1`
